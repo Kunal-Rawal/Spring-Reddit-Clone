e0616be7bf077d69d83727a3d62548aebd631ba8
Kunal-Rawal <kunalrawal.dev@gmail.com>
2024-08-09 19:15:40 +0530

    Initial Commit

---
+++ src/main/java/com/springredditclone/controller/PostController.java
@@ -0,0 +1,55 @@
+package com.springredditclone.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springredditclone.model.PostRequest;
+import com.springredditclone.serviceImpl.PostServiceImpl;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+	@Autowired
+	private PostServiceImpl postServiceImpl;
+	
+	@GetMapping("/getAllPosts")
+	public List<PostRequest> getAllPosts(){
+		return postServiceImpl.getAllPosts();
+	}
+	
+	@PostMapping("/createPost")
+	public PostRequest createPost(@RequestBody PostRequest postRequest){
+		return postServiceImpl.createPost(postRequest);
+	}
+	
+	@GetMapping(params = "subRedditName")
+	public List<PostRequest> getPostsBySubRedditName(@RequestParam String subRedditName) {
+		return postServiceImpl.getPostBySubreddit(subRedditName);
+	}
+	
+	@GetMapping("/{id}")
+	public PostRequest getPost(@PathVariable long id) {
+		return postServiceImpl.getPost(id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public PostRequest deletePost(@PathVariable long id) {
+		return postServiceImpl.deletePost(id);
+	}
+	
+	@PutMapping("/{id}")
+	public PostRequest updatePost(@PathVariable long id, @RequestBody PostRequest postRequest) {
+		return postServiceImpl.updatePost(id,postRequest);
+	}
+}
---
+++ src/main/java/com/springredditclone/model/PostRequest.java
@@ -0,0 +1,60 @@
+package com.springredditclone.model;
+
+
+public class PostRequest {
+
+	private Long postId;
+	private String subRedditName;
+	private String postName;
+	private String url;
+	private String description;
+	
+	@Override
+	public String toString() {
+		return "PostRequest [postId=" + postId + ", subRedditName=" + subRedditName + ", postName=" + postName
+				+ ", url=" + url + ", description=" + description + "]";
+	}
+	public PostRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public PostRequest(Long postId, String subRedditName, String postName, String url, String description) {
+		super();
+		this.postId = postId;
+		this.subRedditName = subRedditName;
+		this.postName = postName;
+		this.url = url;
+		this.description = description;
+	}
+	public Long getPostId() {
+		return postId;
+	}
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+	public String getSubRedditName() {
+		return subRedditName;
+	}
+	public void setSubRedditName(String subRedditName) {
+		this.subRedditName = subRedditName;
+	}
+	public String getPostName() {
+		return postName;
+	}
+	public void setPostName(String postName) {
+		this.postName = postName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
